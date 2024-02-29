1204b88bd561e9b36340e7386949110f5ddbc90f
Raja Kumar <rajakumarmahto952@gmail.com>
2024-02-29 22:35:41 +0530

    adding routing and middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,90 +1,16 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign, verify, decode } from "hono/jwt";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
 
-const app = new Hono<{
+export const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
 }>();
 
-app.use("/api/v1/blog/*", async (c, next) => {
-  const jwt = c.req.header("Authorization");
-  if (!jwt) {
-    c.status(401);
-    return c.json({ error: "unauthorized" });
-  }
-  const token = jwt.split(" ")[1];
-  const payload = await verify(token, c.env.JWT_SECRET);
-  if (!payload) {
-    c.status(401);
-    return c.json({ error: "unauthorized" });
-  }
-  c.set("jwtPayload", { userId: payload.id });
-  await next();
-});
-
-app.post("/api/v1/blog", (c) => {
-  console.log(c.get("jwtPayload"));
-  return c.text("signin route");
-});
-
-app.post("/api/v1/signup", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-      },
-    });
-    const jwt = await sign({ userId: user.id }, c.env.JWT_SECRET);
-    return c.json({ jwt });
-  } catch (e) {
-    c.status(403);
-    return c.json({ error: "error while signing up" });
-  }
-});
-
-app.post("/api/v1/signin", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  const user = await prisma.user.findUnique({
-    where: {
-      email: body.email,
-      password: body.password,
-    },
-  });
-
-  if (!user) {
-    c.status(403);
-    return c.json({ error: "user not found" });
-  }
-
-  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-  return c.json({ jwt });
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("get blog route");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("signin route");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("signin route");
-});
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app;
 
@@ -92,3 +18,11 @@ export default app;
 
 // db connection pool
 // DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMTdkM2QzODUtMmI2Ny00NWFkLWFlZGItYTRkNGJmODc2MzNhIiwidGVuYW50X2lkIjoiZDUxY2VjMDE1ZTRkMGU1YTM2ZThiZWI1ZDQ4ODM1NGM4NDgwODU4ZWE1ZmY0NTVlODZiMTg1ZmQ4YjdmM2M3YSIsImludGVybmFsX3NlY3JldCI6IjNhNzhhNWVmLTAzMjEtNDQwOS04ZDYyLTM0ZjYxYjcxZTEzYiJ9.wx_xIOtxPfUxooHPpVQwcQ7BWiF3SmbOhCSHqKTPlAw"
+
+// React (FE)
+// Cloudflare workers (FE)
+// zod as the validation library, type inference for the frontend types
+// Typescript as a language
+// Prisma as the ORM, with connection pooling
+// Postgres as the database
+// jwt for authentication (cookies)
---
+++ backend/src/middleware.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { verify } from "hono/jwt";
+
+export const middleware = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+middleware.use("/api/v1/blog/*", async (c, next) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const jwt = c.req.header("Authorization");
+  if (!jwt) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  c.set("userId", payload.id);
+  await next();
+});
+
+export default middleware;
